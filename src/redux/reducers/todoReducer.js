e093c19f65e01ae486e9eb8f61f8c7ed14b6eab1
Anton Kiilo <anton.kiilo@wsa-097-74b.icx.local>
2021-04-28 12:33:13 +0300

    rewrote set to spread

--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,4 +1,4 @@
-import { createReducer } from 'typesafe-actions';
+import { action, createReducer } from 'typesafe-actions';
 import * as R from 'ramda';
 import {
   addTodo,
@@ -12,17 +12,20 @@ const initialState = {};
 export const todoReducer = createReducer(initialState)
   .handleAction(addTodo, (state, action) => R.assoc(action.payload.id, action.payload, state))
   .handleAction(removeTodo, (state, action) => R.dissoc(action.payload, state))
-  .handleAction(toggleTodoStatus, (state, action) =>
-    R.set(
-      R.lensPath([action.payload, 'isCompleted']),
-      !state[action.payload].isCompleted,
-      state,
-    ),
-  )
-  .handleAction(editTodo, (state, action) => 
-      R.set(
-        R.lensPath([action.payload.id, 'content']),
-        action.payload.text,
-        state
-      )
-  );
+  .handleAction(toggleTodoStatus, (state, action) => ({...state, [action.payload]: {...state[action.payload], isCompleted: !state[action.payload].isCompleted}}))
+  .handleAction(editTodo, (state, action) => ({...state, [action.payload.id]: {...state[action.payload.id], content: action.payload.text}}))
+  
+  
+  // R.set(
+  //   R.lensPath([action.payload, 'isCompleted']),
+  //   !state[action.payload].isCompleted,
+  //   state,
+  // ),
+  
+  // .handleAction(editTodo, (state, action) => 
+  //     R.set(
+  //       R.lensPath([action.payload.id, 'content']),
+  //       action.payload.text,
+  //       state
+  //     )
+  
